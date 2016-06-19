bd3cea16e4bb1015f5e63d3a444eb27dde3d3555
Wei Jun <junster90@gmail.com>
2016-06-19 16:34:37 +0800

    used fetch method for commentaries response

--- lib/soccerama/client/commentaries.rb
+++ lib/soccerama/client/commentaries.rb
@@ -2,7 +2,7 @@ module Soccerama
   class Client
     module Commentaries
       def commentaries(match_id)
-        get("/commentaries/match/#{match_id}")['commentaries']
+        get("/commentaries/match/#{match_id}").fetch('commentaries')
       end
     end
   end
